7b900a2446adde86f696488acff59174259806fc
betalantz <betalantz@gmail.com>
2021-11-04 13:48:40 -0700

    adds comments for Big O quiz

--- phase-2/algo-club-p2/big-o-analysis.js
+++ phase-2/algo-club-p2/big-o-analysis.js
@@ -1,5 +1,11 @@
+// generates an array of 40000 random integers
 const testArr = [...Array(40000)].map(()=> Math.floor(Math.random() * (1 - 1 + 999) + 1))
 
+/*
+Below are 3 functions which all take an unsorted array of numbers and return the smallest number from the array.
+
+What is the time complexity of each algorithm expressed in Big O?
+*/
 
 function findSmallest1(arr){
     let smallest
