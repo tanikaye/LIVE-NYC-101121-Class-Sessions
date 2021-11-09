54dfcd72906ccb82fae5b33969aabdb5537570bf
betalantz <betalantz@gmail.com>
2021-11-09 15:35:59 -0800

    GETs and renders all pokemon

--- phase-2/react-hooks-practice-pokemon-searcher/src/components/PokemonCard.js
+++ phase-2/react-hooks-practice-pokemon-searcher/src/components/PokemonCard.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({ pokemon: {name, hp, sprites} }) {
+  // const { name, hp, sprites} = pokemon
   return (
     <Card>
       <div>
         <div className="image">
-          <img alt="oh no!" />
+          <img src={sprites.front} alt="oh no!" />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp} hp
           </span>
         </div>
       </div>
--- phase-2/react-hooks-practice-pokemon-searcher/src/components/PokemonCollection.js
+++ phase-2/react-hooks-practice-pokemon-searcher/src/components/PokemonCollection.js
@@ -2,10 +2,11 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemon }) {
+  const pokeCards = pokemon.map(pokeObj => <PokemonCard key={pokeObj.id} pokemon={pokeObj}/>)
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {pokeCards}
     </Card.Group>
   );
 }
